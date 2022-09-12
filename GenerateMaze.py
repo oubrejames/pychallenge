59815c286cbb30b9aebd83e8bbcfc60968eb7a06
Katie Hughes <katiereeshughes@gmail.com>
2022-09-12 10:38:26 -0500

    Maze generation now returns something. Algorithm isn't quite working though.

--- GenerateMaze.py
+++ GenerateMaze.py
@@ -1,6 +1,18 @@
 import numpy as np
 import random
 
+
+class Coord:
+    def __init__(self, r, c):
+        self.r = r
+        self.c = c
+    def p(self):
+        print(f"{self.r}, {self.c}")
+
+def print_coord_list(l):
+    for c in l: 
+        c.p()
+
 class Generate: 
     def __init__(self, m, n):
         self.m = m
@@ -9,14 +21,61 @@ class Generate:
         # Set random cell B to empty
         random_r = random.randrange(0,m)
         random_c = random.randrange(0,n)
-        print(random_r, random_c)
+        cell_b = Coord(random_r, random_c)
+        cell_b.p()
         self.maze[random_r][random_c] = 0
         print(self.maze)
-        wall_list = self.neighbors(random_r, random_c)
-        print(wall_list)
-        #while len(wall_list):
-        #    wall_c = random.choice(wall_list)
-        #    print("WALLC:", wall_c)
+        wall_list = self.neighbors(cell_b)
+        while len(wall_list):
+            print_coord_list(wall_list)
+            wall_c = random.choice(wall_list)
+            wall_c.p()
+            #find the cell A that wall C divides
+            if cell_b.r == wall_c.r: 
+                print("Same row")
+                if cell_b.c > wall_c.c: 
+                    print("Decrease col")
+                    new_c = wall_c.c - 1
+                    print(new_c)
+                else: 
+                    print("Increase col")
+                    new_c = wall_c.c + 1
+                    print(new_c)
+                cell_a = Coord(cell_b.r, new_c)
+            elif cell_b.c == wall_c.c:
+                print("Same col")
+                if cell_b.r > wall_c.r: 
+                    print("Decrease row")
+                    new_r = wall_c.r - 1
+                    print(new_r)
+                else: 
+                    print("Increase row")
+                    new_r = wall_c.r + 1
+                    print(new_r)
+                cell_a = Coord(new_r, cell_b.c)
+            # IF cell_A is a wall (code 1)
+            if (self.maze[cell_a.r][cell_a.c]):
+                # Make cell D whichever one is the wall
+                cell_d = cell_a
+                #Free Cell C
+                self.maze[wall_c.r][wall_c.c] = 0
+                #Free Cell D
+                self.maze[cell_d.r][cell_d.c] = 0
+                #Not sure about this one...
+                cell_b = cell_d
+            elif (self.maze[cell_b.r][cell_b.c]):
+                # Make cell D whichever one is the wall
+                cell_d = cell_b
+                #Free Cell C
+                self.maze[wall_c.r][wall_c.c] = 0
+                #Free Cell D
+                self.maze[cell_d.r][cell_d.c] = 0
+                #Not sure about this one...
+                cell_b = cell_d
+            wall_list.remove(wall_c)
+            print(self.maze)
+            #exit()
+
 
         #for r in range(0,m):
         #    for c in range(0,n):
@@ -26,16 +85,16 @@ class Generate:
         
         #self.start = (,)
         #self.end = (,)
-    def neighbors(self, r, c):
+    def neighbors(self, co):
         lst = []
-        if r < self.m-1: 
-            lst.append((r+1, c))
-        if r > 0: 
-            lst.append((r-1, c))
-        if c < self.n-1: 
-            lst.append((r, c+1))
-        if c > 0: 
-            lst.append((r, c-1))
+        if co.r < self.m-1: 
+            lst.append(Coord(co.r+1, co.c))
+        if co.r > 0: 
+            lst.append(Coord(co.r-1, co.c))
+        if co.c < self.n-1: 
+            lst.append(Coord(co.r, co.c+1))
+        if co.c > 0: 
+            lst.append(Coord(co.r, co.c-1))
         return lst
 
         

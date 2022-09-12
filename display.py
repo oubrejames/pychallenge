87654304f087b7290bf552fb18b5ea9e07c38686
Dilan-Wijesinghe <dilan.wijesinghe0@gmail.com>
2022-09-12 11:28:10 -0500

    Display Map
    Finished displaying the map with images, including a Robot character,
    tiles for the walls and a start and finish flag.

--- display.py
+++ display.py
@@ -7,12 +7,18 @@ import sys
 pygame.init()
 clock = pygame.time.Clock()
 
+# Images
+tile_img = pygame.image.load("images/tile.png")
+start_img = pygame.image.load("images/start.png")
+finish_img = pygame.image.load("images/finish.png")
+robot_img = pygame.image.load("images/robot.png")
+
 # Tile Map Properties
 M = 20
 N = 20
 START = 'Some Index'
 FINISH = 'Another Index'
-TILE_SZ = 32 # Hard Coded, will edit to tile.get_width()
+TILE_SZ =  tile_img.get_width() # Hard Coded, will edit to tile.get_width()
 
 # Screen Properties 
 screen = pygame.display.set_mode((M * TILE_SZ,N * TILE_SZ), 0)
@@ -20,9 +26,15 @@ pygame.display.set_caption("a-MAZE-ing Solver!")
 BkgdColor = (139, 200, 254)
 # ObjectColor = (43, 39, 32)
 
+# Player Information
+robot_loc = [50,50]
+
+# Numpy Conversion
+# nparray.tolist()
+
 
 # Game Map
-game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+game_map = [['2','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
@@ -36,12 +48,12 @@ game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
             ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
-            ['1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1','1','0'],
-            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'],
-            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'],
-            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'],
-            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'],
-            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0']]
+            ['1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','3']]
 
 # Booleans
 running = True
@@ -49,6 +61,25 @@ running = True
 # Game Loop
 while running:
     screen.fill(BkgdColor) # Fill the Display with chosen background 
+
+    # Displaying Game Map
+    tiles = []
+    y = 0
+    for row in game_map:
+        x = 0
+        for tile in row:
+            if tile == '1':
+                screen.blit(tile_img, (x * TILE_SZ, y * TILE_SZ))
+            if tile == '2':
+                screen.blit(start_img, (x * TILE_SZ, y * TILE_SZ))
+            if tile == '3':
+                screen.blit(finish_img, (x * TILE_SZ, y * TILE_SZ))
+            if tile != '0':
+                tiles.append(pygame.Rect(x * TILE_SZ, y * TILE_SZ, TILE_SZ, TILE_SZ))
+            x += 1 
+        y += 1 
+        
+    screen.blit(robot_img, robot_loc) # Displaying the Robot 
 
     for event in pygame.event.get():
          if event.type == QUIT:

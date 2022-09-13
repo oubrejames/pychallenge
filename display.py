3493a67c28c1e4d150f58a062fa00188745c891a
Dilan-Wijesinghe <dilan.wijesinghe0@gmail.com>
2022-09-13 10:13:28 -0500

    Display Changes
    Removed unused variables, and cleaned up general code. Made the display
    a function called run(). Will add comments in a later commit.

--- display.py
+++ display.py
@@ -27,21 +27,17 @@ Maze = Generate(M,N)
 game_map = Maze.maze
 game_map = game_map.tolist()
 
+# Get Solved Path Coordinates
+# TODO: add code for path coordinates casting to list
+
 # Screen Properties 
 screen = pygame.display.set_mode((M * TILE_SZ,N * TILE_SZ), 0)
 pygame.display.set_caption("a-MAZE-ing Solver!")
 BkgdColor = (139, 200, 254)
-# ObjectColor = (43, 39, 32)
 
 # Location Information
-# robot_loc = [50,50]
 start_loc = [Maze.start.r, Maze.start.c]
 end_loc = [Maze.end.r, Maze.end.c]
-prev_loc = []
-item = 0
-loop_num = 1
-
-
 
 game_map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 0, 0, 1, 0], 
@@ -69,13 +65,16 @@ path_coords =[[9,15],[8,15],[7,15],[7,14],[7,13],[7,12],[7,11],[7,10],
               [6,3],[7,3],[8,3],[9,3],[10,3],[11,3],[12,3],[13,3],[13,2],[13,1]]
 
 
-# print(path_coords[2][1])
 # Add location of start and end into the game_map
 # game_map[start_loc[0]][start_loc[1]] = 2
 # game_map[end_loc[0]][end_loc[1]] = 3
 
 # Change game_map to a list of strings
 game_map = [list(map(str,x)) for x in game_map]
+
+# Change path_coords to a list of ints
+# path_coords = [list(map(int,x)) for x in path_coords]
+# path_coords = path_coords.tolist()
 
 # Method for adding 1 to every element in the game_map
 # test = [list(map(int,x)) for x in game_map]
@@ -87,66 +86,65 @@ def MoveRobot(PathCoords):
     y = PathCoords[0]
 
     # When I eventually fix the points
-    # x = path_coords[i][0] # Get X and Y of Path
-    # y = path_coords[i][1]
+    # x = path_coords[0] # Get X and Y of Path
+    # y = path_coords[1]
 
     game_map[x][y] = '5' # Update to path_img
 
-    # pygame.display.update()
-
-
 # Booleans
 running = True
-path_flag = False
 
-# Game Loop
-while running:
-    screen.fill(BkgdColor) # Fill the Display with chosen background 
+def run(loop_num):
+    item = 0
+    loop_num = 1
+    # Game Loop
+    while running:
+        screen.fill(BkgdColor) # Fill the Display with chosen background 
 
-    # Displaying Game Map
-    tiles = []
-    y = 0
-    for row in game_map:
-        x = 0
-        for tile in row:
-            if tile == '1':
-                screen.blit(tile_img, (x * TILE_SZ, y * TILE_SZ))
-            if tile == '2':
-                screen.blit(start_img, (x * TILE_SZ, y * TILE_SZ))
-            if tile == '3':
-                screen.blit(finish_img, (x * TILE_SZ, y * TILE_SZ))
-            # if tile == '4':
-            #     screen.blit(path_img, (x * TILE_SZ, y * TILE_SZ))
-            if tile == '5':
-                screen.blit(robot_img, (x * TILE_SZ, y * TILE_SZ))
-            if tile != '0':
-                tiles.append(pygame.Rect(x * TILE_SZ, y * TILE_SZ, TILE_SZ, TILE_SZ))
-            x += 1 
-        y += 1 
-    
-    # Want to Display the Path only after a set time (1 second delay)
-    click_time = pygame.time.get_ticks() / 1000
-    go_time = (click_time) % (1 * loop_num)
-    # print(click_time, go_time)
+        # Displaying Game Map
+        tiles = []
+        y = 0
+        for row in game_map:
+            x = 0
+            for tile in row:
+                if tile == '1':
+                    screen.blit(tile_img, (x * TILE_SZ, y * TILE_SZ)) # Tile
+                if tile == '2':
+                    screen.blit(start_img, (x * TILE_SZ, y * TILE_SZ)) # Start Flag
+                if tile == '3':
+                    screen.blit(finish_img, (x * TILE_SZ, y * TILE_SZ)) # End Flag
+                # if tile == '4':
+                #     screen.blit(path_img, (x * TILE_SZ, y * TILE_SZ))
+                if tile == '5':
+                    screen.blit(robot_img, (x * TILE_SZ, y * TILE_SZ)) # Little Robot
+                if tile != '0':
+                    tiles.append(pygame.Rect(x * TILE_SZ, y * TILE_SZ, TILE_SZ, TILE_SZ))
+                x += 1 
+            y += 1 
         
-    if go_time <= 0.01999: # Change hardcoded time delay (currently 5)
-        loop_num += 1
-        if item >= len(path_coords) - 1:
-           item = len(path_coords) - 1
+        # Want to Display the Path only after a set time (1 second delay)
+        click_time = pygame.time.get_ticks() / 1000
+        go_time = (click_time) % (1 * loop_num)
+        # print(click_time, go_time)
+            
+        if go_time <= 0.01999:
+            loop_num += 1
+            if item >= len(path_coords) - 1:
+                item = len(path_coords) - 1
 
-        MoveRobot(path_coords[item])
-        if item == 0:
-            prev_loc = path_coords[item]
-            print(prev_loc)
-        item += 1
+            MoveRobot(path_coords[item])
+            # if item == 0:
+            #     prev_loc = path_coords[item]
+            #     print(prev_loc)
+            item += 1
         
-    # screen.blit(robot_img, robot_loc) # Displaying the Robot 
+        # Closing the Game
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            
+        pygame.display.update()
+        clock.tick(60) # Run at specified fps
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        
-    
-    pygame.display.update()
-    clock.tick(60) # Run at specified fps
+run(1)
